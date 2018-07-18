870bd94ef033d0bd201d5592b5498bf1c19cd63c
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-18 16:43:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now(hours) > 12
+    if Time.now(hour) > 12
       resp.write "Good Morning!"
     else
       resp.write "Good Morning!"
     end
-    
+
     resp.end
   end
 
